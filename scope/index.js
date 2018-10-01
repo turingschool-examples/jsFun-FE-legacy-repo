ce3259f9559a47fbcd1bd10ede21aa423831c9da
Brittany Storoz <brittanystoroz@gmail.com>
2018-10-01 15:06:38 -0600

    Add scopes

--- scope/index.js
+++ scope/index.js
@@ -68,7 +68,171 @@ const scope = {
 
     // Annotation:
     // Write your annotation here as a comment
+  },
 
+  exerciseC() {
+    let greeting = 'Hello';
+
+    function greetingFunction() {
+      var greeting = 'Yo';
+
+      if (greeting === 'Yo') {
+        let greeting = 'Howdy';
+      }
+
+      // Log A: greeting
+
+      function newPhrase() {
+        greeting = 'Hey';
+
+        // Log B: greeting
+      }
+
+      newPhrase();
+
+      // Log C: greeting
+    }
+
+    greetingFunction();
+
+    // Log D: greeting
+
+    const result = 'REPLACE WITH YOUR RESULT HERE';
+    return result;
+
+    // Annotation:
+    // Write your annotation here as a comment
+  },
+
+  exerciseD() {
+    let greeting = 'howdy';
+
+    const greetingGenerator = () => {
+      let greeting = 'hi';
+
+      if (greeting === 'hi') {
+        let greeting = 'hello';
+      }
+
+      // Log A: greeting
+
+      const newGreeting = ()  => {
+        greeting = 'welcome';
+
+        // Log B: greeting
+      }
+
+      newGreeting();
+
+      // Log C: greeting
+    }
+
+    greetingGenerator();
+
+    // Log D: greeting
+
+    const result = 'REPLACE WITH YOUR RESULT HERE';
+    return result;
+
+    // Annotation:
+    // Write your annotation here as a comment
+  },
+
+  exerciseE() {
+    let name = 'Brittany';
+
+    function sayName() {
+      let name = 'Pam';
+
+      if (name === 'Pam') {
+        name = 'Nathaniel';
+
+        if (name.length > 0) {
+          let name = 'Brittany';
+        }
+
+        // Log A: name
+      }
+
+      // Log B: name
+    }
+
+    // Log C: name
+
+    sayName();
+
+    // Log D: name
+
+    const result = 'REPLACE WITH YOUR RESULT HERE';
+    return result;
+
+    // Annotation:
+    // Write your annotation here as a comment
+  },
+
+  exerciseF() {
+    var dog = 'Spot';
+
+    function petDog() {
+      // Log A: dog
+
+      if (dog === 'Spot') {
+        let dog = 'Fluffy';
+      }
+
+      function rollOver() {
+        // Log B: dog
+
+        dog = 'Biscuit';
+
+        // Log C: dog
+
+      }
+
+      rollOver();
+
+      // Log D: dog
+    }
+
+    petDog();
+
+    // Log E: dog
+
+    const result = 'REPLACE WITH YOUR RESULT HERE';
+    return result;
+
+    // Annotation:
+    // Write your annotation here as a comment
+  },
+
+  exerciseG() {
+    var fruit = 'apple';
+
+    function eatFruit() {
+
+      if (fruit !== 'kiwi') {
+        var fruit = 'mango';
+
+        if (fruit) {
+          // Log A: fruit
+          const fruit = 'strawberry';
+        }
+
+        // Log B: fruit
+      }
+
+      // Log C: fruit
+    }
+
+    eatFruit()
+
+    // Log D: fruit
+
+    const result = 'REPLACE WITH YOUR RESULT HERE';
+    return result;
+
+    // Annotation:
+    // Write your annotation here as a comment
   }
 }
 
