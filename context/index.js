8da357e74dc1396c49bc39c3862dcad4bf706038
Eric Weissman <eric.weissman21@gmail.com>
2018-11-28 12:01:55 -0700

    Added before instructor changes

--- context/index.js
+++ context/index.js
@@ -16,11 +16,12 @@ const context = {
 
 
     // What is the value of `this` when we call ship.fly()?
-    const result = 'REPLACE WITH YOUR RESULT HERE';
+    const result = 'global window object';
     return result;
 
     // Annotation:
-    // Write your annotation here as a comment
+    // 'this' will reference the global window object because 'this' is first created using an arrow function, in this case, with the fly function. Since arrow functions are a part of ES6, 'this' is set when the function is created.  Since the fly function is not a method on an object and is used AFTER the initialization of fly on the SpaceProbe class, 'this' will refer to the global window object. 
+
   },
 
   exerciseB() {
@@ -30,11 +31,11 @@ const context = {
     }
     
     // What is the value of `this` when we call fn()?
-    const result = 'REPLACE WITH YOUR RESULT HERE';
+    const result = 'global window object';
     return result;
 
     // Annotation:
-    // Write your annotation here as a comment
+    // 'this' will reference  the global window object because exerciseB creates a new function using ES5 that references 'this', however, since it is written using ES5 - 'this' won't be set until the function is called. In exerciseB, the function fn() hasn't been called yet, which is why 'this' refers to the global window object.
   },
 
   exerciseC() {
@@ -49,11 +50,11 @@ const context = {
     el.addEventListener('click', car.getInfo);
 
     // What is the value of `this` when a user clicks on our element and car.getInfo() is triggered?
-    const result = 'REPLACE WITH YOUR RESULT HERE';
+    const result = 'el';
     return result;
 
     // Annotation: 
-    // Write your annotation here as a comment
+    // 'this' is in reference to the constant el. For this example, 'this' refers to the object that the method addEventListener function is applied to. This example uses ES5, whereby 'this' is set when the function is EXECUTED on line 50. 
   },
 
   exerciseD() {
@@ -66,15 +67,16 @@ const context = {
     
         return innerFunction();
       }
-    };
+    }; 
 
 
     // What is the value of `this` when we call dog.getBreed()?
-    const result = 'REPLACE WITH YOUR RESULT HERE';
+    const result = 'global window object';
     return result;
 
     // Annotation: 
-    // Write your annotation here as a comment
+    // 'this' will reference the global window object. In this example, 'this' is set using ES5 when the function is called on line 68 - since the innerFunction has parens at the end. Since this is called before we have a chance to apply the getBreed method on the dog object, 'this' refers to the global window object. ** Functions within functions lose reference to this without any type of binding (adding this at the end with a comma), so it defaults to the global window object
+    
   },
 
   exerciseE() {
@@ -86,11 +88,11 @@ const context = {
 
 
     // What is the value of `this` when we call fn()?
-    const result = 'REPLACE WITH YOUR RESULT HERE';
+    const result = 'global window object';
     return result;
 
     // Annotation: 
-    // Write your annotation here as a comment
+    // 'this' refers to the global window object because it lives in the global space. It isn't a function as a method on an object, and it is not used with new operator. 
   },
 
   exerciseF() {
@@ -109,11 +111,12 @@ const context = {
     const storm = new Hero('Ororo', 'weather control', true);
 
     // What is the value of `this` when we call storm.identifyHero()?
-    const result = 'REPLACE WITH YOUR RESULT HERE';
+    const result = 'instance of Hero';
     return result;
 
     // Annotation: 
-    // Write your annotation here as a comment
+  //  This refers to the instance of Hero (which is storm) as this follows rule 3 - this in function code invoked using the new operator (on line 111) refers to the newly created object  
+  
   },
 
   exerciseG() {
@@ -137,11 +140,11 @@ const context = {
 
 
     // What is the value of `this` when we call monopoly.restart()?
-    const result = 'REPLACE WITH YOUR RESULT HERE';
+    const result = 'global window object';
     return result;
 
     // Annotation: 
-    // Write your annotation here as a comment
+    // 'this' refers to the global window object because it is called within another function within the restart method. Since this is not a method on the Game object, it lives within its execution context, so this defaults to the global object window. 
   },
 
   exerciseH() {
@@ -157,11 +160,11 @@ const context = {
     obj.method();
 
     // What is the value of `this` when we call obj.arrowFunction()?
-    const result = 'REPLACE WITH YOUR RESULT HERE';
+    const result = 'obj';
     return result;
 
     // Annotation: 
-    // Write your annotation here as a comment
+    // 'this' refers to obj because it utilizes ES6 syntax when it reassigns the arrowFunction property (line 152) to an actual arrow function. In ES6, once the arrow function is declared, 'this' is set, and in this example, 'this' will refer to the obj object.
   },
 
   exerciseI() {  
@@ -180,10 +183,12 @@ const context = {
     }, poets);
 
     // What is the value of `this` that gets returned on each iteration of poets.map()?
-    const result = 'REPLACE WITH YOUR RESULT HERE';
+    const result = 'poets';
     return result;
 
     // Annotation: 
+    // 'this' refers to the poets object because poets is used as the thisArg as the second argument in the map prototype. The thisArg is the value to use as 'this' when executing the callback (which is on line 181). 
+    
     // Write your annotation here as a comment. Annotation should include explanation regarding the second argument of `poets` that is being passed
   },
 
@@ -194,21 +199,21 @@ const context = {
     });
 
     // What is the value of `this` when a user clicks on our #btn element and the callback is triggered?
-    const result = 'REPLACE WITH YOUR RESULT HERE';
+    const result = 'el';
     return result;
 
     // Annotation: 
-    // Write your annotation here as a comment.
+    // 'this' refers to el as outlined by rule two. It is part of the .on method which is associated with el, which is an object on the dom. 
   },
 
   exerciseK() {
     const el = $('#btn');
     el.on('click', () => {
       console.log(this);
-    });
+    }); 
 
     // What is the value of `this` when a user clicks on our #btn element and the callback is triggered?
-    const result = 'REPLACE WITH YOUR RESULT HERE';
+    const result = 'el';
     return result;
 
     // Annotation: 
