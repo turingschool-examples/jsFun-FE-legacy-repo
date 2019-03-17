7585b3c3b0baa5502641b04df5edcdda6a57a75c
Rachael Drennan <39016273+rdren0@users.noreply.github.com>
2019-03-17 17:57:01 -0600

    Create doctorWho.js

---
+++ prototypes/datasets/doctorWho.js
@@ -0,0 +1,49 @@
+const doctorWho = {
+  rose: {
+    lastName: 'tyler',
+    doctor: ['Nineth', 'Tenth'],
+    episodes: 35,
+    companions: ['Jackie Tyler', 'Mickey Smith']
+  },
+  donna: {
+    lastName: 'noble',
+    doctor: ['Tenth'],
+    episodes: 17,
+    companions: ['Wilfred Noble']
+  },
+  river: {
+    lastName: 'song',
+    doctor: ['Tenth', 'Eleventh', 'Twelfth'],
+    episodes: 19,
+    companions: ['Amy Pond', 'Rory Williams']
+  },
+  martha: {
+    lastName: 'jones',
+    doctor: ['Tenth'],
+    episodes: 25,
+    companions: ['Mickey Smith']
+  },
+  clara: {
+    lastName: 'oswald',
+    doctor: ['Eleventh', 'Twelfth'],
+    episodes: 41,
+    companions: ['Danny Pink', 'Ashildr Me']
+  },
+  amy: {
+    lastName: 'pond',
+    doctor: ['Eleventh', 'Twelfth'],
+    episodes: 38,
+    companions: ['Rory Willaims', 'Brian Willaims']
+  },
+  bill: {
+    lastName: 'potts',
+    doctor: ['Twelfth'],
+    episodes: 14,
+    companions: ['Nardole']
+  }
+}
+
+module.exports = {
+doctorWho
+}
+
