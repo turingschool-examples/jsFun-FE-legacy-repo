16d173e622eb497e40143ec1eca149936b3f905d
Scott Ertmer <scottertmer@gmail.com>
2022-01-24 11:26:59 -0700

    Add puppers.js to increase practice with args and params

---
+++ prototypes/datasets/puppers.js
@@ -0,0 +1,26 @@
+const puppers = [
+  {
+    name: 'Scout',
+    age: 12,
+    color: 'grey'
+  },
+  {
+    name: 'Hatchet',
+    age: 3,
+    color: 'orange'
+  },
+  {
+    name: 'Stick',
+    age: 6,
+    color: 'brown'
+  },
+  {
+    name: 'Butter',
+    age: 1,
+    color: 'Orange'
+  }
+];
+
+module.exports = {
+  puppers
+}
